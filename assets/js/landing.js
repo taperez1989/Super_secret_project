9c26a95ff0718da0c443975364daf6649f0a2e1e
Irvin Lu <scurvyirv@scurvyirvy.local>
2024-05-08 20:49:04 -0700

    this commit updates the ninja API

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -6,3 +6,25 @@
         menu.classList.toggle('is-active');
     });
 })();
+
+
+// Ninja API for workout data fetch
+const workoutSearchEntry = document.querySelector('.workout-entry'); //replace with respective HTML class/ID
+const workoutSubmit = document.querySelector('.workout-submit'); //replace with HTML
+const apiWorkoutKey = 'qn/zcJQkQfpyU7iVcOwjfg==jdIi92gLftIqjh63';
+
+//API call to populate workouts based on search entry/muscle group
+function getNinjaApi(event) {
+    event.preventDefault();
+    //fetch request
+    const requestNinjaUrl = `https://api.api-ninjas.com/v1/exercises?muscle=${workoutSearchEntry.value}&limit=10&appid=${apiWorkoutKey}`;
+
+    fetch(requestNinjaUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            
+        })
+}
