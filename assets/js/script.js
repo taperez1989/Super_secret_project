f0ef2c5a8db4679f4864b73c786423157ea40ede
Ted Perez <taperez1989@yahoo.com>
2024-05-09 18:49:41 -0700

    created local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,23 @@ const gymSearch = document.querySelector('.search-button');
 gymSearch.addEventListener('click', function (event) {
     event.preventDefault();
 
-    const cityName = document.querySelector('.search-input').value;
+    const searchedCity = document.querySelector('.search-input').value;
 
-    if (cityName === '') {
+    if (searchedCity === '') {
         window.alert('Enter City');
 
         return;
     }
+
+    const cities = {
+        searchedCity,
+    };
+
+    const city = JSON.parse(localStorage.getItem("city")) || [];
+
+    city.push(cities);
+
+    localStorage.setItem("city", JSON.stringify(city));
 
     location.href = "landing.html";
 });
